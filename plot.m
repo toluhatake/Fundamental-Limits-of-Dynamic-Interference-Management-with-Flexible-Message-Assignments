bb21396e48ebcb60be5adf66b83dff8ffb78e612
toluhatake <40046206+toluhatake@users.noreply.github.com>
2018-07-10 17:30:53 -0400

    Update plot.m

--- plot.m
+++ plot.m
@@ -1,5 +1,5 @@
 clc
-p=linspace(0,1,10000);  %probability vector
+p=linspace(0,1,100);  %probability vector
 [DoFMax,H,msg]=montecarlo(6000);
 
 A1=2*p+(1-(1-p).^2+p.*(1-p).^3).*(1+(1-p).^2);
@@ -18,4 +18,4 @@ plot(p, DoF_Theorem_4, 'green','LineWidth',2);hold all;
 plot(p, DoF_Theorem_5, 'blue','LineWidth',2);
 plot(p, DoFMax, 'red','LineWidth',2);
 legend(...'1/6', '1/5', '3/10', '1/7', ...'numeric 1/6',...'analytic 2/6',  ... 'analytic 1/6 new', ...'analytic 4/5', ...'analytic 1/5', ...
-     'K = 5, f(p) = 3/5', 'K \rightarrow \infty, f(p) = 0', 'maximum DoF'); xlabel('p'); ylabel('puDoF');
+     'f(p) = 3/5', 'f(p) = 0', 'Best assignment'); xlabel('p'); ylabel('puDoF');
